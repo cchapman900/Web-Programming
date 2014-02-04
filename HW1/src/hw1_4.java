02b729dc2bcebb9edc296529460edbd74df5233f
Chris Chapman <chrischapman900@gmail.com>
2014-02-03 19:31:07 -0500

    Create hw1_4.java

---
+++ HW1/src/hw1_4.java
@@ -0,0 +1,22 @@
+/*
+* BMCC CIS 485 - Web Programming
+* Spring 2014
+* Chris Chapman
+* HW1
+*
+* hw1_4 Menu Based Application
+*
+* Displays main menu to select either Rectangle
+* or Square problem.
+*
+*/
+
+public class hw1_4 extends hw1 {
+
+public static void main(String[] args) {
+
+rectangleMainMenu();
+
+}
+
+}
